f2207a5a58b259bbdb19a1ca0a0779b601fc7534
Sebastian Schneid <sschneid12@gmail.com>
2026-07-26 12:53:52 +0200

    Turnierinfos ergänzt

--- app/page.tsx
+++ app/page.tsx
@@ -91,12 +91,15 @@ className="max-w-5xl mx-auto px-6 py-24 space-y-12"
 
 <div className="bg-zinc-900 rounded-2xl p-6">
 <h3 className="text-2xl font-bold mb-4">📅 Datum</h3>
-<p>folgt</p>
+<p>21.08.206
+    <br />
+    <strong>Turnierbeginn:</strong> 18:30 Uhr 
+   <strong>Anmeldung vor Ort:</strong> 17:30 Uhr  </p>
 </div>
 
 <div className="bg-zinc-900 rounded-2xl p-6">
 <h3 className="text-2xl font-bold mb-4">📍 Ort</h3>
-<p>folgt</p>
+<p>Bauwagen Laub />Dorfstraße 1</p>
 </div>
 
 <div className="bg-zinc-900 rounded-2xl p-6">
